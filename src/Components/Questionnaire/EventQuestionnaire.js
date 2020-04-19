3997b7964f9fe4d55652fd601854fdb404af9fa6
Jose Cuentas <43096276+jcuentas@users.noreply.github.com>
2020-04-18 21:34:19 -0400

    save answers implemented for survey and questionnaire
    also fixed grphical glitch with survey

--- src/Components/Questionnaire/EventQuestionnaire.js
+++ src/Components/Questionnaire/EventQuestionnaire.js
@@ -4,6 +4,7 @@ import { getQuestionnaire } from "../Questionnaire/QuestionReader";
 import "survey-react/survey.css";
 import * as Survey from "survey-react";
 import EventDashboard from "../EventDashboard/EventDashboard";
+import Axios from "axios";
 
 class EventQuestionnaire extends Component {
   constructor(props) {
@@ -26,6 +27,79 @@ class EventQuestionnaire extends Component {
     this.props.onComplete();
   };
 
+<<<<<<< Updated upstream
+=======
+    onCompleteComponent = (survey) =>{
+        this.setState({
+            isCompleted: true
+        })
+
+        console.log("Survey results: " + JSON.stringify(survey.data));
+        this.sendToServer(survey.data);
+    }
+
+    sendToServer = (data) => {
+        var keyArray = Object.keys(data);
+        var answerId = this.parseToAnswerId(keyArray, data);
+        for(var i = 0; i < answerId.length; i++){
+            var json = {
+                candidate : parseInt(this.state.userId),
+                question : parseInt(keyArray[i]),
+                answer : parseInt(answerId[i])
+            }
+            this.postAnswerToDb(json);
+
+        }
+
+        
+    }
+
+    postAnswerToDb = (answerJson) => {
+        const url = 'http://localhost:8000/api/answer/';
+        Axios.post(url,answerJson).then((response)=>{
+          console.log("answer has been saved");
+        }, (error)=>{
+          console.log(error);
+        });
+    }
+
+    parseToAnswerId = (keyArray, data) => {
+        var answerId = [];
+        for(var i = 0; i < keyArray.length; i++){
+            var questionObj = getFromApi("question", keyArray[i]);
+            for(var j = 0; j < 5; j++){
+                if(j == 0) {
+                    if(data[keyArray[i]] == questionObj.op1){
+                        answerId.push(j);
+                        break;
+                    }
+                } else if( j == 1) {
+                    if(data[keyArray[i]] == questionObj.op2){
+                        answerId.push(j);
+                        break;
+                    }
+                } else if( j == 2) {
+                    if(data[keyArray[i]] == questionObj.op3){
+                        answerId.push(j);
+                        break;
+                    }
+                } else if( j == 3) {
+                    if(data[keyArray[i]] == questionObj.op4){
+                        answerId.push(j);
+                        break;
+                    }
+                } else if( j == 4) {
+                    if(data[keyArray[i]] == questionObj.op5){
+                        answerId.push(j);
+                        break;
+                    }
+                }
+            }
+        }
+        return answerId;
+    }
+
+>>>>>>> Stashed changes
   render() {
     Survey.StylesManager.applyTheme("darkblue");
 
